aaa6269bcd98956f2b53fbef17edeac693c2c313
Myrsini Gkolemi <mirtoseri@gmail.com>
2024-11-09 21:03:26 +0100

    Add external network and creation to setup

--- setup.sh
+++ setup.sh
@@ -6,5 +6,8 @@ mkdir -p config/sysconfig
 # First configure the .env files
 python configure_env.py
 
+# Create external network to skip the naming issues
+docker network create chn-network
+
 # Then compose
 docker compose up -d
